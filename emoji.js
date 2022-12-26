5c5489f68be6638561ccb35f04d84e31cd1a0ffb
lalafatima33 <82950609+lalafatima33@users.noreply.github.com>
2022-12-26 22:26:13 +0100

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,22 +1,21 @@
-const starsEl = document.querySelectorAll('.fa-star')
-const emojisEl = document.querySelectorAll('.far')
-const colorsArray = ['red', 'orange', 'lightblue', 'lightgreen', 'green']
-updateRating(0)
-starsEl.forEach((starEl, index) => {
-  starEl.addEventListener('click', () => {
-   updateRating(index)
+const fig = document.querySelectorAll('.far')
+let stella = document.querySelectorAll('.fa-star')
+stella.forEach((stellina, index) => {
+  stellina.addEventListener('click', ()=> {
+    updateRating(index)
   })
 })
-function updateRating(index) {
-  starsEl.forEach((starEl, idx)=> {
-if(idx < index +1){
-  starEl.classList.add('active')
+function  updateRating(index){
+  stella.forEach((stellina, idx)=> {
+if(idx < index + 1) {
+  stellina.classList.add('active')
 }else{
-  starEl.classList.remove('active')
+  stellina.classList.remove('active')
 }
+
+})
+fig.forEach((figi) => {
+  figi.style.transform = `translateX(-${index * 50}px)`
   })
-  emojisEl.forEach(emojiEl => {
-    emojiEl.style.transform = `translateX(-${index * 50}px)`
-    emojiEl.style.color = colorsArray[index]
-  })
+
 }
